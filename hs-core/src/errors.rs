ab90623f5f5cc236e4ac4d0ae2c432158ace9c6c
sromero <sromero0889@gmail.com>
2024-03-05 10:25:45 +0100

    moved model weights to other repo & added logic to download it when build

--- hs-core/src/errors.rs
+++ hs-core/src/errors.rs
@@ -4,14 +4,12 @@
 pub enum HsError {
     #[error("Assets Not found, Model {0}")]
     ModelAssetsNotFound(&'static str),
-    #[error("Init Model {1} failed: {0}")]
-    InitModelError(#[from] candle_core::Error),
-    // #[error("Init Model {1} failed: {0}")]
-    // InitModelError(&'static str, &'static str),
+    #[error("Init Model failed")]
+    InitModelError(#[source] candle_core::Error),
     #[error("Error mapping json file")]
     JsonMapError(#[from] serde_json::Error),
     #[error("Error mapping safetensors file")]
-    SafetensorsMapError(#[from] candle_core::Error),
+    SafetensorsMapError(#[source] candle_core::Error),
     #[error(transparent)]
     Other(#[from] anyhow::Error),
 }
---
+++ hs-mm-embeddings/build.rs
@@ -0,0 +1,37 @@
+use std::path::{Path, PathBuf};
+
+struct RemoteModel {
+    pub url: &'static str,
+    pub name: &'static str,
+    pub filename: &'static str,
+}
+const M_CLIP_VIT_B_32: RemoteModel = RemoteModel {
+    url: "https://raw.githubusercontent.com/sromero0889/test_models/blob/main/clip_vit_b_32/model.safetensors",
+    name: "clip_vit_b_32",
+    filename: "model.safetensors",
+};
+
+fn download_model_file(model_info: &RemoteModel) {
+    use std::{fs::File, fs::create_dir_all, io::copy};
+    let dir = Path::new("models").join(model_info.name);
+
+    let dst_path = &dir.join(model_info.filename);
+    if !dst_path.exists() {
+        println!("download_model {}", model_info.name);
+        let mut response = reqwest::blocking::get(model_info.url).unwrap();
+
+        create_dir_all(&dir).unwrap();
+        let mut file = File::create(dst_path).unwrap();
+        copy(&mut response, &mut file).unwrap();
+    }
+}
+// Important: unwrap use here for now: Build does not make any sense without these files, I want it to panic
+// todo improve errors before expose lib
+
+fn main() {
+    if cfg!(feature = "clip_vit_b_32") {
+        download_model_file(&M_CLIP_VIT_B_32);
+    };
+    // TODO add condition here to download model every time you add a new one
+
+}
