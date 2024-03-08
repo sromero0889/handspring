a39fe6621961a4f5d831e0a710cae14fcd8763ad
sromero <sromero0889@gmail.com>
2024-03-08 13:58:44 +0100

    Audio Core main structure

---
+++ hs-audio-core/src/embeddings/model.rs
@@ -0,0 +1,3 @@
+// use hs_core::transformers::layers::{TransformerModelGen, TransformerResBlock};
+// use crate::embeddings::layers::AudioEmbedLayer;
+// pub type VisionTransformer = TransformerModelGen<AudioEmbedLayer, TransformerResBlock>;
